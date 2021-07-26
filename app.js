7a7ae8c7e2751a27be3e290bcfcb3104f5cb27a8
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-07-26 19:54:03 +0530

    updated JS

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 "use strict";
 
-const billAmt=document.querySelector("#billAmt");
-const cashAmt=document.querySelector("#cashAmt");
-let balance=cashAmt-billAmt;
+const bill = document.querySelector("#billAmt");
+const cash = document.querySelector("#cashAmt");
+
+// console.log(`this is the balance${balance}`);
+
+const message = document.querySelector(".message");
+const nextBtn = document.querySelector("#next");
+const submit = document.querySelector("#submit");
 
 
-function clickHandler(){
+function nextfn(){
+    const billAmt = Number(bill.value);
 
-    if(balance===0){
+    if(billAmt<0){
         
     }
 }
+
+
+
+
+console.log("hi");
+
+
+
+
+
+
+
+
+
+
+function clickHandler() {
+    const cashAmt = Number(cash.value);
+    let balance = cashAmt - billAmt;
+    // console.log(billAmt, cashAmt);
+    // console.log(balance);
+    if(balance===0){
+        message.classList.remove("hidden");
+        message.innerText="There is no Balance";
+    }
+
+};
+
+submit.addEventListener("click", clickHandler);
