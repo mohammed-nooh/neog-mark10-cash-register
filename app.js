cb5e864fddacbc0f46a544b692fb700e5cade33a
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-07-26 22:58:12 +0530

    updated buttons

--- app.js
+++ app.js
@@ -6,40 +6,41 @@ const cash = document.querySelector("#cashAmt");
 // console.log(`this is the balance${balance}`);
 
 const message = document.querySelector(".message");
-const nextBtn = document.querySelector("#next");
-const submit = document.querySelector("#submit");
+const nextBtn = document.querySelector(".nextBtn");
+const submit = document.querySelector(".subBtn");
+
+function showError(text){
+    message.classList.remove("hidden");
+    message.innerText=text;
+}
 
 
 function nextfn(){
     const billAmt = Number(bill.value);
 
-    if(billAmt<0){
-        
+    if(billAmt<1){
+        showError("Enter valid Bill");
+    }
+    else{
+        message.classList.add("hidden");
+        nextBtn.style.display="none";
+        cash.classList.remove("hidden");
     }
 }
 
-
+nextBtn.addEventListener("click",nextfn);
 
 
 console.log("hi");
 
-
-
-
-
-
-
-
-
-
 function clickHandler() {
+    // const billAmt = Number(bill.value);
     const cashAmt = Number(cash.value);
     let balance = cashAmt - billAmt;
     // console.log(billAmt, cashAmt);
     // console.log(balance);
     if(balance===0){
-        message.classList.remove("hidden");
-        message.innerText="There is no Balance";
+        showError("No balance");
     }
 
 };
