ff1dede009cadad00e72ee8fb3af49aa42f6f114
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-07-26 12:37:20 +0530

    updated CSS

--- app.js
+++ app.js
@@ -2,4 +2,12 @@
 
 const billAmt=document.querySelector("#billAmt");
 const cashAmt=document.querySelector("#cashAmt");
+let balance=cashAmt-billAmt;
 
+
+function clickHandler(){
+
+    if(balance===0){
+        
+    }
+}
